701b0214a4ba1283bdf8b1eb47f676c37bdef203
zhangchao <sweetwisdom@qq.com>
2022-02-21 21:39:17 +0800

    实现组件

--- 01.基础/learing03/src/index.js
+++ 01.基础/learing03/src/index.js
@@ -1,27 +1,50 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-class Header extends Component {
-  constructor() {
-    super();
-    this.isGood = true;
+class Title extends Component {
+  titleClick(e) {
+    console.log(e.target, "this is Title");
   }
   render() {
-    const name = "zccc";
-
+    return <h1 onClick={this.titleClick}>React 小书</h1>;
+  }
+}
+class Header extends Component {
+  render() {
     return (
       <div>
-        <h1
-          onClick={() => {
-            console.log("123");
-          }}
-        >
-          React {name}
-        </h1>
-        {this.isGood ? <strong>is good 🤣 </strong> : <span> is good 😀</span>}
+        <Title />
+        <h2>This is Header</h2>
       </div>
     );
   }
 }
-
-ReactDOM.render(<Header />, document.getElementById("root"));
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is main content</h2>
+      </div>
+    );
+  }
+}
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is footer</h2>
+      </div>
+    );
+  }
+}
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
+}
+ReactDOM.render(<Index />, document.getElementById("root"));
