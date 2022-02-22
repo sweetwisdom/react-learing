dabe3decc7437afcf67b05b9057a3556d595d522
zhangchao <sweetwisdom@qq.com>
2022-02-22 22:48:13 +0800

    实现第二种方法❤️

--- 01.基础/learing03/src/List.js
+++ 01.基础/learing03/src/List.js
@@ -1,3 +1,4 @@
+// 这是第二种方法
 function List() {
   const users = [
     { username: "Jerry", age: 21, gender: "male" },
@@ -5,9 +6,9 @@ function List() {
     { username: "Lily", age: 19, gender: "female" },
     { username: "Lucy", age: 20, gender: "female" },
   ];
-  const userEl = [];
-  for (const user of users) {
-    userEl.push(
+
+  const userEl = function (user) {
+    return (
       <div>
         <p>姓名：{user.username}</p>
         <p>年龄：{user.age}</p>
@@ -15,7 +16,29 @@ function List() {
         <hr></hr>
       </div>
     );
-  }
-  return <div>{userEl}</div>;
+  };
+
+  return <div>{users.map((user) => userEl(user))}</div>;
 }
+
+// function List() {
+//   const users = [
+//     { username: "Jerry", age: 21, gender: "male" },
+//     { username: "Tomy", age: 22, gender: "male" },
+//     { username: "Lily", age: 19, gender: "female" },
+//     { username: "Lucy", age: 20, gender: "female" },
+//   ];
+//   const userEl = [];
+//   for (const user of users) {
+//     userEl.push(
+//       <div>
+//         <p>姓名：{user.username}</p>
+//         <p>年龄：{user.age}</p>
+//         <p>性别：{user.gender}</p>
+//         <hr></hr>
+//       </div>
+//     );
+//   }
+//   return <div>{userEl}</div>;
+// }
 export default List;
