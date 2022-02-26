650cfd7c981ac1e7ec1954c65b753234f5b827e2
zhangchao <sweetwisdom@qq.com>
2022-02-26 23:28:18 +0800

    完善样式

--- 01.基础/comment-app/src/CommentInput.js
+++ 01.基础/comment-app/src/CommentInput.js
@@ -1,7 +1,25 @@
-import React, { Component } from "react";
-class CommentInput extends Component {
-  render() {
-    return <div>CommentInput</div>;
-  }
+import React from "react";
+// rcc rfc
+export default function CommentInput() {
+  return (
+    <div className="comment-input">
+      <div className="comment-field">
+        <span className="comment-field-name">用户名：</span>
+        <div className="comment-field-input">
+          <input type="text" />
+        </div>
+      </div>
+      {/* okk  评论区域 */}
+      <div className="comment-field">
+        <span className="comment-field-name">评论内容：</span>
+        <div className="comment-field-input">
+          <textarea name="comment" id="" cols="30" rows="10"></textarea>
+        </div>
+      </div>
+      {/* 发布按钮 */}
+      <div className="commnet-field-button">
+        <button>发布</button>
+      </div>
+    </div>
+  );
 }
-export default CommentInput;
--- 01.基础/comment-app/src/CommentList.js
+++ 01.基础/comment-app/src/CommentList.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 class CommentList extends Component {
   render() {
-    return <div>CommentList</div>;
+    return (
+      <div className="commnet-input">
+        <div className="class-field"></div>
+      </div>
+    );
   }
 }
 export default CommentList;
