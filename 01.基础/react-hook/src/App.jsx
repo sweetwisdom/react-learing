39e77d362defdd4dd2fc2fe9e1521587c8898d3d
zhangchao <sweetwisdom@qq.com>
2022-08-27 12:44:19 +0800

    add

---
+++ 01.基础/react-hook/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="useState">
+      <h2>react hook {count}</h2>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        add
+      </button>
+    </div>
+  );
+}
+
+export default App;
+
