b0d2fed340372f4b6b2a440b0b74fa9c7cc3c9b9
zhangchao <sweetwisdom@qq.com>
2022-08-30 10:12:54 +0800

    完成路由跳转

--- 02.进阶/react-router/src/App.jsx
+++ 02.进阶/react-router/src/App.jsx
@@ -1,7 +1,26 @@
 // 内置组件
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-const Home = () => <h2>Home</h2>;
-const About = () => <h2>About</h2>;
+import { BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom";
+const Home = () => {
+  document.title = "home";
+  const navigate = useNavigate();
+  // 路由跳转函数
+  return (
+    <div>
+      <h2>Home</h2>
+      <button onClick={() => navigate("/about")}> about</button>
+    </div>
+  );
+};
+const About = () => {
+  const navigate = useNavigate();
+  document.title = "about";
+  return (
+    <div>
+      <h2>About</h2>
+      <button onClick={() => navigate("/")}> home</button>
+    </div>
+  );
+};
 function App() {
   return (
     <div className="App">
